684eb9f3397119521dcde005ca31ffc903393b4f
midoI <ahmed.jardak@gmail.com>
2016-03-28 13:07:17 +0200

    create an example for using json serializer

---
+++ Exam-Ref-70-483/Exam-Ref-70-483/Chapter4ImplementDataAccess/Objective4.4SerializeaAndDeserializeData/UsingJsonSerializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Exam_Ref_70_483.Chapter4ImplementDataAccess.Objective4._4SerializeaAndDeserializeData
+{
+	public class UsingJsonSerializer
+	{
+		public static void Execute()
+		{
+			var p = new Person
+			{
+				Id = 1,
+				Name = "John Doe"
+			};
+			using (var stream = new MemoryStream())
+			{
+				var ser = new DataContractJsonSerializer(typeof (Person));
+				ser.WriteObject(stream, p);
+				stream.Position = 0;
+				var streamReader = new StreamReader(stream);
+				Console.WriteLine(streamReader.ReadToEnd()); // Displays {“Id”:1,”Name”:”John Doe”}
+				stream.Position = 0;
+				var result = (Person) ser.ReadObject(stream);
+			}
+		}
+	}
+
+	[DataContract]
+	public class Person
+	{
+		[DataMember]
+		public int Id { get; set; }
+
+		[DataMember]
+		public string Name { get; set; }
+	}
+}
---
+++ Exam-Ref-70-483/Exam-Ref-70-483/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Exam_Ref_70_483.Chapter4ImplementDataAccess.Objective4._4SerializeaAndDeserializeData;
+
+namespace Exam_Ref_70_483
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			UsingJsonSerializer.Execute();
+			Console.Read();
+		}
+	}
+}
